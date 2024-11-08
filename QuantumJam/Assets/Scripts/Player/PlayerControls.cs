766a70f37e5179c1d743475dffca335abf59cc84
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-11-07 23:31:09 -0800

    implement visual on tile hover

--- QuantumJam/Assets/Scripts/Player/PlayerControls.cs
+++ QuantumJam/Assets/Scripts/Player/PlayerControls.cs
@@ -4,18 +4,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject hoverOverlay;
+
     private Vector3 mouseWorldPosition;
     private GameBoardManager gbm;
 
 
-
     private void Start() {
         gbm = GameBoardManager.Instance;
+        hoverOverlay.SetActive(false);
         
     }
 
     private void Update() {
         mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        HoverTile();
         DetectLeftClick();
     }
 
@@ -26,6 +30,18 @@ public class PlayerControls : MonoBehaviour
         if (hit.collider && hit.collider.CompareTag("Tile")) {
             gbm.SelectTile(hit.collider.gameObject);
         };
+    }
+
+    private void HoverTile() {
+        Ray clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.GetRayIntersection(clickRay);
+        if (hit.collider && hit.collider.CompareTag("Tile")) {
+            hoverOverlay.transform.position = hit.collider.transform.position;
+            // play sfx
+            hoverOverlay.SetActive(true);
+        } else {
+            hoverOverlay.SetActive(false);
+        }
     }
 
 }
